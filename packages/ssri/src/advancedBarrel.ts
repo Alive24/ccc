ce5bd29b52c6e378cc31cdada62af004ad677631
Alive24 <xct24@live.com>
2024-11-22 19:19:09 +0000

    feat: CCC's support for SSRI (WIP)

---
+++ packages/ssri/src/advancedBarrel.ts
@@ -0,0 +1,1 @@
+export * from "./ssri/advanced.js";
---
+++ packages/ssri/src/barrel.ts
@@ -0,0 +1,6 @@
+export * from "./ssri/index.js";
+export * from "./udt/index.js";
+export * from "./udtExtended/index.js";
+export * from "./udtMetadata/index.js";
+export * from "./udtPausable/index.js";
+export * from "./utils/index.js";
