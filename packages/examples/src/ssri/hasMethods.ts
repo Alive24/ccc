182ad090a97ac5012033d1d4ed58d17deb618f46
Alive24 <xct24@live.com>
2025-02-11 07:44:26 +0000

    feat: updated examples

--- packages/examples/src/ssri/hasMethods.ts
+++ packages/examples/src/ssri/hasMethods.ts
@@ -3,26 +3,26 @@ import { signer } from "@ckb-ccc/playground";
 
 const executor = new ccc.ssri.ExecutorJsonRpc("http://localhost:9090");
 
-const pudtScriptCell = await signer.client.findSingletonCellByType({
+const scriptCell = await signer.client.findSingletonCellByType({
   // TypeID Code Hash. Don't change
   codeHash:
     "0x00000000000000000000000000000000000000000000000000545950455f4944",
   hashType: "type",
-  // TypeID args. Change it to the args of the Type ID script of your UDT
+  // TypeID args. Change it to the args of the Type ID script of your Script
   args: "0x8fd55df879dc6176c95f3c420631f990ada2d4ece978c9512c39616dead2ed56",
 });
-if (!pudtScriptCell) {
-  throw new Error("PUDT script cell not found");
+if (!scriptCell) {
+  throw new Error("Script cell not found");
 }
 
-const pudtCodeHash = pudtScriptCell.cellOutput.type?.hash();
-if (!pudtCodeHash) {
-  throw new Error("PUDT code hash not found");
+const scriptCodeHash = scriptCell.cellOutput.type?.hash();
+if (!scriptCodeHash) {
+  throw new Error("Script code hash not found");
 }
 
-const pudt = new ccc.ssri.Trait(pudtScriptCell.outPoint, executor);
+const script = new ccc.ssri.Trait(scriptCell.outPoint, executor);
 
-const pudtHasMethods = await pudt.hasMethods([
+const scriptHasMethods = await script.hasMethods([
   "SSRI.get_methods",
   "SSRI.has_methods",
   "SSRI.version",
@@ -39,7 +39,7 @@ const pudtHasMethods = await pudt.hasMethods([
   "UNKNOWN",
 ]);
 
-console.log(pudtHasMethods);
+console.log(scriptHasMethods);
 ///
 // {"res":[true,true,true,true,true,true,true,true,true,true,true,true,true,false],"cellDeps":[]}
 ///
--- packages/examples/src/udt/decimals.ts
+++ packages/examples/src/udt/decimals.ts
@@ -3,7 +3,7 @@ import { signer } from "@ckb-ccc/playground";
 
 const executor = new ccc.ssri.ExecutorJsonRpc("http://localhost:9090");
 
-const pudtScriptCell = await signer.client.findSingletonCellByType({
+const udtScriptCell = await signer.client.findSingletonCellByType({
   // TypeID Code Hash. Don't change
   codeHash:
     "0x00000000000000000000000000000000000000000000000000545950455f4944",
@@ -11,27 +11,27 @@ const pudtScriptCell = await signer.client.findSingletonCellByType({
   // TypeID args. Change it to the args of the Type ID script of your UDT
   args: "0x8fd55df879dc6176c95f3c420631f990ada2d4ece978c9512c39616dead2ed56",
 });
-if (!pudtScriptCell) {
-  throw new Error("PUDT script cell not found");
+if (!udtScriptCell) {
+  throw new Error("udt script cell not found");
 }
 
-const pudtCodeHash = pudtScriptCell.cellOutput.type?.hash();
-if (!pudtCodeHash) {
-  throw new Error("PUDT code hash not found");
+const udtCodeHash = udtScriptCell.cellOutput.type?.hash();
+if (!udtCodeHash) {
+  throw new Error("udt code hash not found");
 }
-const pudtType = {
-  codeHash: pudtCodeHash,
+const udtType = {
+  codeHash: udtCodeHash,
   hashType: "type",
   args: "0x02c93173368ec56f72ec023f63148461b80e7698eddd62cbd9dbe31a13f2b330",
 };
 
-const pudt = new ccc.udt.Udt(pudtScriptCell.outPoint, pudtType, {
+const udt = new ccc.udt.Udt(udtScriptCell.outPoint, udtType, {
   executor,
 });
 
-const pudtDecimals = await pudt.decimals();
+const udtDecimals = await udt.decimals();
 
-console.log(pudtDecimals);
+console.log(udtDecimals);
 ///
 // {"res":"6","cellDeps":[]}
 ///
